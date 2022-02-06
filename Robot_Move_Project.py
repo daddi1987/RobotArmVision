6b80764209a63063c2b4bc177aa211b57b017d13
daddi1987 <39605171+daddi1987@users.noreply.github.com>
2022-02-06 17:57:19 +0100

    Update Robot_Move_Project.py

--- Robot_Move_Project.py
+++ Robot_Move_Project.py
@@ -2,35 +2,23 @@ from RobotArmMove import RobotArm
 import time
 from Hand_tools import HeadAndTools
 
-
 # Open GRBL serial port
-# Example for Raspberry serial.Serial('/dev/tty.usbmodem1811',115200)
 Robot = RobotArm("COM4",115200)
-#RobotArmSerial = serial.Serial(port="COM4", baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=None)
-
 print("Initializing RobotArm")
+
 # Wait for grbl to initialize and flush startup text in serial input
 # Wake up grbl
 
+# module control Hand
 Hand = HeadAndTools("COM5")
 
-Hand.setHead(8,90,160)
+Hand.setHead(8,0,90)
 
 Hand.OpenGripper()
-Hand.CloseGripper()
-Hand.OpenGripper()
-Hand.CloseGripper()
-
-
-#Define Firmata Software
-#HendRobot = ArduinoNano('COM5')
+#Hand.CloseGripper()
 print("Communication Hend Robot successfully")
 
-
-#RobotArmSerial.write(b"\r\n")
 Robot._Carriage_Return()
-#Robot.Write_Value_Setup()
-
 
 print(Robot.CheckConnectionRobot()) #Check Connection Function launch
 
@@ -52,76 +40,44 @@ Robot.CheckParameters() #Go Print Check Parameters
 Robot.ShowHelpMenu() #Go Print Help Menu
 
 Robot.CheckParametersSetup() # Print Parameters Setup
-'''
-i=0
-while i < 5:
-    #RobotArm().GoToPosition(-80,100.1,40.123,1000) #Function GoToPosition
-    Robot.GoToPosition(-80, 100.1, 40.123, 50000)
-    print("Command GoTOPos End")
 
-    #RobotArm().GoToPosition(0,0,0,10) #Function GoToPosition second move
-    Robot.GoToPosition(0,0,0,2000)
-    print("Command GoTOPos End")
+OffsetYBlock = 30
+OfsetZBlock = -10
+PositionYForBlockDown = 245
+PositionZForBlockDown = -110
+NumberBlock = 2-1 # Tolto 1 per posizione bassa
+i = 0
+while i<2:
 
-    #RobotArm().GoToPosition(80,100.1,40.123,1000) #Function GoToPosition
-    Robot.GoToPosition(80,100.1,40.123,1500)
-    print("Command GoTOPos End")
-
-    Robot.GoToPosition(0,0,0,50000)
-    print("Command GoTOPos End")
-
-    Robot.GoToPosition(0, 100.1, 0, 5000)
-    print("Command GoTOPos End")
-
-    #RobotArm().GoToPosition(0,0,0,10) #Function GoToPosition second move
-    Robot.IncrementalPosition(0,10,10,50000)
-    print("Command GoTOPos End")
-
-    Robot.IncrementalPosition(0,-10,-10,1500)
-    print("Command GoTOPos End")
-
-    Robot.GoToPosition(0, 0, 0, 5000)
-    print("Command GoTOPos End")
-
-    i= i+1
-
-'''
-Robot.GoToPosition(0, 60, 0, 50000)
-print("Command GoTOPos End")
-
-Robot.GoToPosition(0, 60, -50, 50000)
-
-Robot.GoToPosition(-130, 60, -50, 50000)
-
-Robot.GoToPosition(-130, 150, -50, 50000)
-
-Robot.GoToPosition(-130, 150, -110, 50000)
-
-Robot.GoToPosition(-130, 250, -110, 50000)
-
-time.sleep(3)
-
-Robot.GoToPosition(-130, 100, -110, 50000)
-
-Robot.GoToPosition(-130, 100, -50, 50000)
-
-Robot.GoToPosition(0, 100, -50, 50000)
-
-Robot.GoToPosition(0, 0, 0, 50000)
+    NumberBlock = NumberBlock - i;
+    PositionYRobotPick = OffsetYBlock * NumberBlock
+    PositionYRobotPick = PositionYForBlockDown - PositionYRobotPick
 
 
-'''
- Example for blink led 13 Arduino Nano
- while True:
-        HendRobot.digital[13].write(1)
-        time.sleep(1)
-        HendRobot.digital[13].write(0)
-        time.sleep(1)
-'''
-#https://www.youtube.com/watch?v=8j3Fo-16Rr8
-'''
-HendRobot.digital[8].mode = SERVO
-HendRobot.digital[8].write(90)
-time.sleep(2)
-HendRobot.digital[8].write(10)
-'''
+    # Position Ready
+    Robot.GoToPosition(0, 70, 0, 10000)
+    Robot.GoToPosition(0, 70, -50, 10000)
+
+    #Move W Axis to Target
+    Robot.GoToPosition(-120, 70, -50, 2000)
+
+    Robot.GoToPosition(-120, 150, -50, 50000)
+
+    Robot.GoToPosition(-120, 150, -110, 50000)
+
+    Robot.GoToPosition(-120, PositionYRobotPick, -110, 50000) #Y240 previus Z-110
+
+    time.sleep(2)
+    Hand.CloseGripper()
+
+    Robot.GoToPosition(-120, 100, -110, 50000)
+
+    Robot.GoToPosition(-120, 100, -50, 50000)
+
+    Robot.GoToPosition(0, 100, -50, 50000)
+
+    Robot.GoToPosition(0, 0, 0, 50000)
+
+    Hand.OpenGripper()
+
+    i = i+1 #Increment Cycle
